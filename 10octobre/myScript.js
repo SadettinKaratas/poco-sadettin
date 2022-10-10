c9f4019c54d285a29e9f2609673f75a1198540db
sadettin <sadettin.karatas@powercoders.org>
2022-10-10 15:50:46 +0200

    2

---
+++ 10octobre/myScript.js
@@ -0,0 +1,16 @@
+
+var x = 10;//comment this is my firt value
+var firstVumber = 10;
+// var myFirstSentence = "Hello Worrld!"
+// var bool = ture;
+
+// const pi= 3.222 //assign variables
+
+// let name=12; // assign variables
+
+
+console.log("my first print");
+
+const sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+
+console.log(sampleStr);
