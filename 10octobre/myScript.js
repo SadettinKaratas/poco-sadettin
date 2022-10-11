5466f89881cc9df5f37fbbccd8d4c4e6f71cfc55
sadettin <sadettin.karatas@powercoders.org>
2022-10-11 13:35:21 +0200

    JS

--- 10octobre/myScript.js
+++ 10octobre/myScript.js
@@ -10,6 +10,65 @@
 // document.getElementById("button") = "Hello Dolly.";
 
 
-function myfunction (p1, p2) {
-    return p1 * p2;}
-document.write(myfunction(4,10000))
+// function myfunction (p1, p2) {
+//     return p1 * p2;}
+// document.write(myfunction(4,10000))
+
+// let myBirthYear = 1988;
+// let futureYear = 2026;
+// var age = futureYear - myBirthYear  
+// document.write("I will be either " + age + " or " + (age-1) + " in " + futureYear)
+
+// let myValue = 10;
+
+// if (myValue > 12 || myValue == 12){
+//     document.write("My value is equal to 12");}
+// else{ window.alert("Check your number")} 
+
+// if(myValue < 12){
+//     document.write("Second if works")
+// }
+
+// let myBirthYear = 1988;
+// let myBirthMonth = 11;
+// let futureYear = 2030;
+// let futureMonth = 12;
+
+// if(myBirthMonth > futureMonth) {
+//     document.write(`Your age is ${futureYear - myBirthYear}  . `) 
+// }else{
+//     document.write(`Your age is   ${(futureYear - myBirthYear) + 1} . `)
+// }
+
+// Ternary Conditions
+const yourage = 26;
+
+const beverage = yourage >=25 ? "Beer" : "Tea"
+
+document.write (beverage)
+
+let age = 17;
+let result;
+
+result = age >=18 ? "User can view" : "User can not view"
+document.write(result)
+
+// Switch Condition
+
+let fruit = "banana";
+
+switch (fruit){
+    case "apple":
+    alert(`Your fruit is apple`)
+    
+    break;
+   
+    case "banana":
+    alert(`Your fruit is banana`)
+
+    break;
+
+    default:
+     alert(`You have no fruit`)
+    
+}
