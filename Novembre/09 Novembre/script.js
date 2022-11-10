db4bf1fe3d3704188209f50446a24847bc49c045
sadettin <sadettin.karatas@powercoders.org>
2022-11-10 21:18:34 +0100

    JS Exercices

---
+++ Novembre/09 Novembre/script.js
@@ -0,0 +1,24 @@
+let character = "t";
+let timesToRepeat = 5;
+let myString = "";
+
+for (let i=0; i<=timesToRepeat; i++){
+    myString+=character
+    
+}
+
+console.log(myString)
+
+
+let aVar = "Hillo";
+
+for (let index = 0; index < 4; index++) {
+   
+    aVar += "Hello"
+    console.log(aVar)
+    if (i=2) {
+        aVar= "Go";
+        
+    }
+}
+console.log(aVar)
